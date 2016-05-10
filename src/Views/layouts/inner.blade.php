bb0d5eefa836dcd2b04232875bf1c5a3d282e4b6
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-05-11 09:09:40 +0930

    configurable side menu

--- src/Views/layouts/inner.blade.php
+++ src/Views/layouts/inner.blade.php
@@ -13,38 +13,57 @@
 @section('main-content')
 	<div class="col-sm-2 sidebar">
 		<ul class="nav nav-pills nav-stacked">
+			@if(!config("soda.menu.pages", true))
 			<li class="nav-item">
 				<a class="nav-link {{Request::is('cms/pages*')?'active':''}}"  href="{{route('soda.pages')}}"><span class="fa fa-file-o"></span> Pages</a>
 			</li>
+			@endif
+
+			@if(!config("soda.menu.page-types", true))
 			<li class="nav-item">
-				{{--todo: --}}
 				<a class="nav-link {{Request::is('cms/page-types*')?'active':''}}" href="{{route('soda.page_type')}}"><span class="fa fa-edit"></span> Page Types</a>
 			</li>
+			@endif
+
+			@if(!config("soda.menu.blocks", true))
 			<li class="nav-item">
 				<a class="nav-link {{Request::is('cms/blocks*')?'active':''}}" href="{{route('soda.block')}}"><span class="fa fa-square"></span> Blocks</a>
 			</li>
+			@endif
+
+			@if(!config("soda.menu.block-types", true))
 			<li class="nav-item">
-				{{--todo: --}}
 				<a class="nav-link {{Request::is('cms/block-types*')?'active':''}}" href="{{route('soda.block_type')}}"><span class="fa fa-edit"></span> Block Types</a>
 			</li>
+			@endif
+
+			@if(!config("soda.menu.fields", true))
 			<li class="nav-item">
 				<a class="nav-link {{Request::is('cms/field*')?'active':''}}" href="{{route('soda.field')}}"><span class="fa fa-pencil"></span> Fields</a>
 			</li>
+			@endif
+
+			@if(!config("soda.menu.users", true))
 			<li class="nav-item">
 				<a class="nav-link {{Request::is('cms/user*')?'active':''}}" href="{{route('soda.user')}}"><span class="fa fa-users"></span> Users</a>
 			</li>
+			@endif
+
+			@if(!config("soda.menu.applications", true))
 			<li class="nav-item">
 				<a class="nav-link {{Request::is('cms/applications*')?'active':''}}" href="#"><span class="fa fa-desktop"></span> Applications</a>
 			</li>
+			@endif
+
+			@if(!config("soda.menu.application-settings", true))
 			<li class="nav-item">
 				<a class="nav-link {{Request::is('cms/settings*')?'active':''}}" href="#"><span class="fa fa-cog"></span> Application Settings</a>
 			</li>
+			@endif
+
 			@foreach( event(new Soda\Events\NavigationWasRendered()) as $item)
 				{!! $item !!}
 			@endforeach
-
-
-
 		</ul>
 	</div>
 	<div class="col-sm-offset-2 col-sm-10 main-content">
