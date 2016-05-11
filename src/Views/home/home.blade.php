95962d2db75bc11c102fe185219177c34664d20b
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-05-11 14:43:07 +0930

    Remove cool text from dashboard because it's to "unprofessional" .-.

--- src/Views/home/home.blade.php
+++ src/Views/home/home.blade.php
@@ -3,10 +3,12 @@
 @section('content')
     <div class="jumbotron home-intro">
         <h1 class="display-3">Welcome to Soda CMS!</h1>
-        <p class="lead">Soda CMS is awesome!</p>
         <hr class="m-y-2">
+        <p class="lead">This is your dashboard.</p>
+        {{--
         <p class="lead">
             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </p>
+        --}}
     </div>
 @stop
