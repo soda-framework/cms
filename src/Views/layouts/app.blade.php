cc7e14ad37a85f87cec9bd58dbc8696d9895971b
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-05-11 14:36:52 +0930

    topnav cleanup

--- src/Views/layouts/app.blade.php
+++ src/Views/layouts/app.blade.php
@@ -11,38 +11,42 @@
             <ul class="nav navbar-nav">
 
                 <li class="nav-item active">
-                    <a class="nav-link" href="{{route('home')}}">Home</a>
+                    <a class="nav-link" href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="/">View Site</a>
+                    <a class="nav-link" href="/" target="_blank">View Site</a>
                 </li>
+                {{--
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
                 </li>
+                --}}
                 @if (Auth::guest())
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('/login') }}">Login</a>
                     </li>
+                    {{--
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('/register') }}">Register</a>
                     </li>
+                    --}}
                 @endif
-            </ul>
-            @if(Auth::check())
-                <div class="pull-xs-right">
-                    <div class="dropdown">
-                        <a href='#' class="btn btn-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{Auth::user()->name}}</a>
+</ul>
+@if(Auth::check())
+<div class="pull-xs-right">
+    <div class="dropdown">
+        <a href='#' class="btn btn-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->username }}</a>
 
-                        <div class="dropdown-menu dropdown-menu-right " aria-labelledby="dropdownMenu1">
-                            <a class='dropdown-item' href="{{ route('logout') }}"><i class="fa fa-btn fa-users"></i>My Settings</a>
-                            <a class='dropdown-item' href="{{ route('logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
-                        </div>
-                    </div>
-                </div>
-            @endif
-        </nav>
-        @yield('main-content')
+        <div class="dropdown-menu dropdown-menu-right " aria-labelledby="dropdownMenu1">
+            {{--<a class='dropdown-item' href="{{ route('logout') }}"><i class="fa fa-btn fa-users"></i> My Settings</a>--}}
+            <a class='dropdown-item' href="{{ route('logout') }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a>
+        </div>
     </div>
-    @yield("footer.js")
+</div>
+@endif
+</nav>
+@yield('main-content')
+</div>
+@yield("footer.js")
 </body>
 </html>
