2d8a3f14682f44111585b232174bfa739c6f9c49
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-05-11 12:43:36 +0930

    update login page

--- src/Views/auth/login.blade.php
+++ src/Views/auth/login.blade.php
@@ -1,64 +1,71 @@
 @extends(config('soda.hint_path').'::layouts.app')
 
 @section('main-content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+    <div class="main-content row">
+        <div class="col-xs-6 col-xs-offset-3">
 
-                <div class="panel panel-default">
-                    <h1 class="panel-heading">Login</h1>
-                    <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ route('login-attempt') }}">
-                            {!! csrf_field() !!}
+            <div class="main-content-inner">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="panel panel-default login-box">
+                            <h1 class="panel-heading">Login</h1>
+                            <div class="panel-body">
+                                <form class="form-horizontal" role="form" method="POST"
+                                      action="{{ route('login-attempt') }}">
+                                    {!! csrf_field() !!}
 
-                            <div class="form-group row">
-                                <label class="col-md-4 control-label">E-Mail Address</label>
+                                    <div class="form-group row">
+                                        <label class="col-md-4 control-label">E-Mail Address</label>
 
-                                <div class="col-md-6">
-                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                        <div class="col-md-8">
+                                            <input type="email" class="form-control" name="email"
+                                                   value="{{ old('email') }}">
 
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
+                                            @if ($errors->has('email'))
+                                                <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                    @endif
-                                </div>
-                            </div>
+                                            @endif
+                                        </div>
+                                    </div>
 
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} row">
-                                <label class="col-md-4 control-label">Password</label>
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} row">
+                                        <label class="col-md-4 control-label">Password</label>
 
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" name="password">
+                                        <div class="col-md-8">
+                                            <input type="password" class="form-control" name="password">
 
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
+                                            @if ($errors->has('password'))
+                                                <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Remember Me
-                                        </label>
+                                            @endif
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
 
-                            <div class="form-group row">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        <i class="fa fa-btn fa-sign-in"></i>Login
-                                    </button>
-
-                                    <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                                </div>
+                                    <div class="form-group row">
+                                        <div class="col-md-6">
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" name="remember"> Remember Me
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <button type="submit" class="btn btn-primary pull-right">
+                                                <i class="fa fa-btn fa-sign-in"></i> Login
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-md-6 col-md-offset-4">
+                                            <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your
+                                                Password?</a>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
