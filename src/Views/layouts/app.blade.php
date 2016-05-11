f858703c7df10f59387693112d9d4de9883bf825
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-05-11 12:21:00 +0930

    wrap in container classes so floats dont collapse body height

--- src/Views/layouts/app.blade.php
+++ src/Views/layouts/app.blade.php
@@ -5,42 +5,44 @@
     @yield('main-header')
 </head>
 <body>
-    <nav class="navbar navbar-fixed-top navbar-dark bg-inverse top-nav">
-        <a class="navbar-brand" href="{{route('home')}}">Soda CMS</a>
-        <ul class="nav navbar-nav">
+    <div class="container-fluid">
+        <nav class="navbar navbar-fixed-top navbar-dark bg-inverse top-nav">
+            <a class="navbar-brand" href="{{route('home')}}">Soda CMS</a>
+            <ul class="nav navbar-nav">
 
-            <li class="nav-item active">
-                <a class="nav-link" href="{{route('home')}}">Home</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="/">View Site</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-            </li>
-            @if (Auth::guest())
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('home')}}">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">View Site</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                    <a class="nav-link" href="#">Contact</a>
                 </li>
-            @endif
-        </ul>
-        @if(Auth::check())
-        <div class="pull-xs-right">
-            <div class="dropdown">
-                <a href='#' class="btn btn-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{Auth::user()->name}}</a>
+                @if (Auth::guest())
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                    </li>
+                @endif
+            </ul>
+            @if(Auth::check())
+                <div class="pull-xs-right">
+                    <div class="dropdown">
+                        <a href='#' class="btn btn-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{Auth::user()->name}}</a>
 
-                <div class="dropdown-menu dropdown-menu-right " aria-labelledby="dropdownMenu1">
-                    <a class='dropdown-item' href="{{ route('logout') }}"><i class="fa fa-btn fa-users"></i>My Settings</a>
-                    <a class='dropdown-item' href="{{ route('logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
+                        <div class="dropdown-menu dropdown-menu-right " aria-labelledby="dropdownMenu1">
+                            <a class='dropdown-item' href="{{ route('logout') }}"><i class="fa fa-btn fa-users"></i>My Settings</a>
+                            <a class='dropdown-item' href="{{ route('logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        @endif
-    </nav>
-    @yield('main-content')
+            @endif
+        </nav>
+        @yield('main-content')
+    </div>
     @yield("footer.js")
 </body>
 </html>
