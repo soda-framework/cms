525622291be775913d6afac939c9372868fff368
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-08-18 15:13:35 +0930

    Tidied HomeController

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,49 +1,36 @@
 <?php namespace Soda\Controllers;
 
-use App\Http\Requests;
-use Illuminate\Http\Request;
-use Auth;
 use App\Http\Controllers\Controller;
+use Auth;
+use Gate;
+use Soda\Events\DashboardWasRendered;
 
 class HomeController extends Controller {
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct() {
 //		$this->middleware('soda');
-	}
+    }
 
-	/**
-	 * Show the application dashboard.
-	 *
-	 * @return Response
-	 */
-	public function getIndex()
-	{
-		//dd(Auth::guard('soda')->user(), \Gate::denies('soda.dashboard'), \Gate::forUser(Auth::guard('soda')->user()));
+    /**
+     * Show the application dashboard.
+     *
+     * @return Response
+     */
+    public function getIndex() {
+        if (Gate::denies('soda.dashboard')) {
+            dd('nope');
+            abort(403);
+        }
 
-		//dd(\Gate::forUser(Auth::guard('soda')->user())->denies('soda.dashboard'));
-		//dd(Auth::guard('soda')->user(), Auth::user());
-		if (\Gate::denies('soda.dashboard')) {
-			dd('nope');
-			abort(403);
+        $dashboard = event(new DashboardWasRendered());
 
-		}
-		$dashboard = event(new \Soda\Events\DashboardWasRendered());
-		if(!empty($dashboard)){
-			return $dashboard[0];
-		}
-		else{
-			return view('soda::home.dashboard');
-		}
-		//dd(event(new \Soda\Events\DashboardWasRendered()));
-		//return event(new Soda\Events\DashboardWasRendered());
-
-
-
-	}
+        if (!empty($dashboard)) {
+            return $dashboard[0];
+        } else {
+            return view('soda::home.dashboard');
+        }
+    }
 
 }
