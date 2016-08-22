4d92b85a4c7e3552200d7c656fdf823130606d81
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-08-22 19:36:07 +0930

    Fix RestrictedController

--- themes/simple/src/Controllers/RestrictedController.php
+++ themes/simple/src/Controllers/RestrictedController.php
@@ -5,7 +5,7 @@ namespace Themes\SodaExample\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller {
+class RestrictedController extends Controller {
 
     public function __construct() {
         $this->middleware('auth');
