3da4d261d10cae238f421fda71a343c0172491da
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-08-22 13:53:55 +0930

    handle project within src directory

--- src/Console/Setup.php
+++ src/Console/Setup.php
@@ -31,6 +31,9 @@ class Setup extends Command {
             $contents = file_get_contents($environment_file_path);
             if (!$this->option('no-database')) {
                 $base_name = str_slug(basename(base_path()));
+                if($base_name = 'src') {
+                    $base_name = str_slug(basename(base_path('../')));
+                }
                 $db_host = $this->anticipate('Database host: ', ['localhost']);
                 $db_name = $this->anticipate('Database table name: ', [$base_name]);
                 $db_user = $this->anticipate('Database user: ', ['root']);
