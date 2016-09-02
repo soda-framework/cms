19954d4b8432646b2b65ca3c235ee20e9e7b1891
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-09-02 12:21:43 +0930

    Styling

--- views/partials/nav.blade.php
+++ views/partials/nav.blade.php
@@ -8,7 +8,10 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="soda-logo"  href="{{ route('soda.home') }}"><img src="/soda/cms/img/sodacms_logowhite.png" /></a>
+            <a class="soda-logo"  href="{{ route('soda.home') }}">
+                <img src="/soda/cms/img/sodacms_droplime.png" />
+                <img src="/soda/cms/img/sodacms_logowhite.png" />
+            </a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
