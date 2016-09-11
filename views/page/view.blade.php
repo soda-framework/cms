ee3ebd37c440e6b752ff65e41594ba37f1be7477
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-09-12 09:16:38 +0930

    Make both submit buttons work

--- views/page/view.blade.php
+++ views/page/view.blade.php
@@ -25,36 +25,40 @@ if((!$model->type || !count($model->type->fields)) && !count($model->blocks) && 
     'description' => $model->description,
 ])
 
-@section('page.main')
+
+@section('main-content')
     <form method="POST" action="{{ route('soda.' . $hint . ($model->id ? '.edit' : '.create'), ['id' => $model->id]) }}">
         {!! csrf_field() !!}
         @if($model->type)
             <input type="hidden" name="page_type_id" value="{{ $model->type->id }}" />
         @endif
         <input type="hidden" name="parent_id" value="{{ $model->parent_id }}" />
-
-        {!! SodaForm::text([
-            "name"        => "Name",
-            "description" => "The name of this page",
-            "field_name"  => 'name',
-        ])->setLayout('soda::partials.inputs.layouts.stacked')->setModel($model) !!}
-
-        {!! SodaForm::text([
-            'name'        => 'Slug',
-            'description' => 'The url of this page',
-            'field_name'  => 'slug',
-        ])->setLayout('soda::partials.inputs.layouts.stacked')->setModel($model) !!}
-
-        {!! SodaForm::toggle([
-            'name'         => 'Published',
-            'field_name'   => 'status',
-            'value'        => Soda\Cms\Components\Status::LIVE,
-            'field_params' => ['on-value' => Soda\Cms\Components\Status::LIVE, 'off-value' => Soda\Cms\Components\Status::DRAFT],
-        ])->setLayout('soda::partials.inputs.layouts.stacked')->setModel($model) !!}
-        <br />
-
-        <input class="btn btn-success" type="submit" value="Save" />
+        @parent
     </form>
+@stop
+
+@section('page.main')
+    {!! SodaForm::text([
+        "name"        => "Name",
+        "description" => "The name of this page",
+        "field_name"  => 'name',
+    ])->setLayout('soda::partials.inputs.layouts.stacked')->setModel($model) !!}
+
+    {!! SodaForm::text([
+        'name'        => 'Slug',
+        'description' => 'The url of this page',
+        'field_name'  => 'slug',
+    ])->setLayout('soda::partials.inputs.layouts.stacked')->setModel($model) !!}
+
+    {!! SodaForm::toggle([
+        'name'         => 'Published',
+        'field_name'   => 'status',
+        'value'        => Soda\Cms\Components\Status::LIVE,
+        'field_params' => ['on-value' => Soda\Cms\Components\Status::LIVE, 'off-value' => Soda\Cms\Components\Status::DRAFT],
+    ])->setLayout('soda::partials.inputs.layouts.stacked')->setModel($model) !!}
+    <br />
+
+    <input class="btn btn-success" type="submit" value="Save" />
 @stop
 
 @section('content.sidebar')
@@ -93,93 +97,85 @@ if((!$model->type || !count($model->type->fields)) && !count($model->blocks) && 
         </li>
         @endpermission
     </ul>
-
-    <form method="POST" action="{{ route('soda.' . $hint . ($model->id ? '.edit' : '.create'), ['id' => $model->id]) }}">
-        {!! csrf_field() !!}
+    <div class="tab-content">
         @if($model->type)
-        <input type="hidden" name="page_type_id" value="{{ $model->type->id }}" />
-        @endif
-        <input type="hidden" name="parent_id" value="{{ $model->parent_id }}" />
-        <div class="tab-content">
-            @if($model->type)
-                <div class="tab-pane" id="tab_page_type_{{ $model->type->id }}" role="tabpanel">
-                    <h3>{{ $model->type->name }}</h3>
-                    <p>{{ $model->type->description }}</p>
-                    <hr />
-                    @if($model->type && $model->type->fields)
-                        @foreach($model->type->fields as $field)
-                            {!! SodaForm::field($field)->setModel(@$page_table)->setPrefix('settings') !!}
-                        @endforeach
-                    @endif
-
-                    <input class="btn btn-success" type="submit" value="Save" />
-                </div>
-            @endif
-            @foreach($model->blocks as $block)
-                @if($block->type->edit_action_type == 'view')
-                    <div class="tab-pane" id="tab_block_{{ $block->id }}" role="tabpanel">
-                        @include($block->type->edit_action, [
-                            'unique' => uniqid(),
-                            'render' => 'card',
-                            'block'  => $block,
-                            'page'   => $model,
-                            'models' => $model->blockModel($block)->paginate(null, ['*'], $block->identifier .'-page')
-                        ])
-                    </div>
-                @endif
-                {{--loads a block into place.. --}}
-            @endforeach
-
-            @permission("live-preview")
-            <div class="tab-pane" id="tab_live" role="tabpanel">
-                <h3>Settings</h3>
-                <p>Use this tab to customise information on the page in a live view</p>
+            <div class="tab-pane" id="tab_page_type_{{ $model->type->id }}" role="tabpanel">
+                <h3>{{ $model->type->name }}</h3>
+                <p>{{ $model->type->description }}</p>
                 <hr />
-                @if($model->slug)
-                    <p>{{ $model->slug }}</p>
-                    <iframe width="100%" height=400 src="{{ $model->slug }}?soda_edit=true"></iframe>
-                @else
-                    <p>You must set a slug to enabled this feature.</p>
+                @if($model->type && $model->type->fields)
+                    @foreach($model->type->fields as $field)
+                        {!! SodaForm::field($field)->setModel(@$page_table)->setPrefix('settings') !!}
+                    @endforeach
                 @endif
-            </div>
-            @endpermission
-
-            @permission("advanced-pages")
-            <div class="tab-pane" id="tab_advanced" role="tabpanel">
-                <h3>Settings</h3>
-                <p>Advanced page settings</p>
-                <hr />
-
-                {!! SodaForm::text([
-                    'name'        => 'Package Name',
-                    'field_name'  => 'package',
-                ])->setModel($model) !!}
-
-                {!! SodaForm::text([
-                    'name'        => 'Action',
-                    'field_name'  => 'action',
-                ])->setModel($model) !!}
-
-                {!! SodaForm::text([
-                    'name'        => 'Action Type',
-                    'field_name'  => 'action_type',
-                ])->setModel($model) !!}
-
-                {!! SodaForm::text([
-                    'name'        => 'Edit Action',
-                    'field_name'  => 'edit_action',
-                ])->setModel($model) !!}
-
-                {!! SodaForm::text([
-                    'name'        => 'Edit Action Type',
-                    'field_name'  => 'edit_action_type',
-                ])->setModel($model) !!}
 
                 <input class="btn btn-success" type="submit" value="Save" />
             </div>
-            @endpermission
+        @endif
+        @foreach($model->blocks as $block)
+            @if($block->type->edit_action_type == 'view')
+                <div class="tab-pane" id="tab_block_{{ $block->id }}" role="tabpanel">
+                    @include($block->type->edit_action, [
+                        'unique' => uniqid(),
+                        'render' => 'card',
+                        'block'  => $block,
+                        'page'   => $model,
+                        'models' => $model->blockModel($block)->paginate(null, ['*'], $block->identifier .'-page')
+                    ])
+                </div>
+            @endif
+            {{--loads a block into place.. --}}
+        @endforeach
+
+        @permission("live-preview")
+        <div class="tab-pane" id="tab_live" role="tabpanel">
+            <h3>Settings</h3>
+            <p>Use this tab to customise information on the page in a live view</p>
+            <hr />
+            @if($model->slug)
+                <p>{{ $model->slug }}</p>
+                <iframe width="100%" height=400 src="{{ $model->slug }}?soda_edit=true"></iframe>
+            @else
+                <p>You must set a slug to enabled this feature.</p>
+            @endif
         </div>
-    </form>
+        @endpermission
+
+        @permission("advanced-pages")
+        <div class="tab-pane" id="tab_advanced" role="tabpanel">
+            <h3>Settings</h3>
+            <p>Advanced page settings</p>
+            <hr />
+
+            {!! SodaForm::text([
+                'name'        => 'Package Name',
+                'field_name'  => 'package',
+            ])->setModel($model) !!}
+
+            {!! SodaForm::text([
+                'name'        => 'Action',
+                'field_name'  => 'action',
+            ])->setModel($model) !!}
+
+            {!! SodaForm::text([
+                'name'        => 'Action Type',
+                'field_name'  => 'action_type',
+            ])->setModel($model) !!}
+
+            {!! SodaForm::text([
+                'name'        => 'Edit Action',
+                'field_name'  => 'edit_action',
+            ])->setModel($model) !!}
+
+            {!! SodaForm::text([
+                'name'        => 'Edit Action Type',
+                'field_name'  => 'edit_action_type',
+            ])->setModel($model) !!}
+
+            <input class="btn btn-success" type="submit" value="Save" />
+        </div>
+        @endpermission
+    </div>
     @endif
 @stop
 
