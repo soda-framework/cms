ed7c9de5ac8333c90993638bfc8e441639608ac4
Ryan Brereton-Canham <ryzaar@hotmail.com>
2016-10-12 12:28:45 +1030

    styling

--- views/partials/tree/base.blade.php
+++ views/partials/tree/base.blade.php
@@ -1,49 +1,70 @@
 <ul class="tree-view">
-	@foreach($tree as $tree_item)
-		@include('soda::partials.tree.tree',['tree'=>$tree_item])
-	@endforeach
+    @foreach($tree as $tree_item)
+        @include('soda::partials.tree.tree',['tree'=>$tree_item])
+    @endforeach
 </ul>
 
 <script>
-	var oldContainer;
-	$("ul.tree-view").sortable({
-		group: 'no-drop',
-		handle: 'i.handle',
-		afterMove: function (placeholder, container) {
-			if(oldContainer != container){
-				if(oldContainer)
-					oldContainer.el.removeClass("active");
-				container.el.addClass("active");
+    var oldContainer;
+    $("ul.tree-view").sortable({
+        group: 'no-drop',
+        handle: '.handle',
+        afterMove: function (placeholder, container, closestItemOrContainer) {
+            if (oldContainer != container) {
+                if (oldContainer) {
+                    oldContainer.el.removeClass("active");
+                }
+                container.el.addClass("active");
 
-				oldContainer = container;
-			}
-		},
-		onDrop: function ($item, container, _super) {
-			var id = $item.data('id');
-			var parent_id = $item.parent().parent().data('id');
-			var position = $item.index();
-			if(typeof parent_id === 'undefined'){
-				parent_id = 'root';
-			}
-			$.get($item.data('move'), {
-						id:id,
-						position:position,
-						parent_id:parent_id
-					},function(){
-				//move complete.
-			});
-			console.log(_super);
-			container.el.removeClass("active");
-			_super($item, container);
-		},
-		serialize: function (parent, children, isContainer) {
-			return isContainer ? children.join() : parent.text();
-		}
-	});
+                oldContainer = container;
+            }
+        },
+        onDrop: function ($item, container, _super) {
+            var id = $item.data('id');
+            var parent = $item.parent().closest('.tree-row');
+            var parent_id = parent.data('id');
+            var position = $item.index();
 
-	$("ul.tree-view .minify").click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('fa-minus').toggleClass('fa-plus');
-		$('ul', $(this).closest('li')).toggle();
-	});
+            if (typeof parent_id === 'undefined') {
+                parent_id = 'root';
+            }
+
+            $.get($item.data('move'), {
+                id: id,
+                position: position,
+                parent_id: parent_id
+            }, function () {
+                //move complete.
+            });
+
+            _super($item, container);
+
+            $('.tree-sub-items:has(li)').closest('.tree-row').children('.tree-item').find('.minify').fadeIn();
+            $('.tree-sub-items:not(:has(li))').closest('.tree-row').children('.tree-item').find('.minify').fadeOut();
+        },
+        serialize: function (parent, children, isContainer) {
+            return isContainer ? children.join() : parent.text();
+        }
+    });
+
+    $(".minify").on('click', function (e) {
+        e.preventDefault();
+        var container = $(this).closest('.tree-row').children('.tree-sub-items');
+        $(this).toggleClass('fa-chevron-down fa-chevron-right');
+
+        container.toggleClass('sub-items-expanded');
+
+        if(container.hasClass('sub-items-expanded')) {
+            container.children().show();
+            container.hide();
+        }
+
+        container.slideToggle(function() {
+            if(!$(this).hasClass('sub-items-expanded')) {
+                $(this).children().hide();
+            }
+
+            $(this).show();
+        });
+    });
 </script>
--- views/partials/tree/tree.blade.php
+++ views/partials/tree/tree.blade.php
@@ -1,22 +1,34 @@
 {{--renders tree in html --}}
-<li data-id="{{$tree->id}}" data-move="{{route('soda.'.$hint.'.move')}}">
-	<div class="tree-item">
-		<i class="fa fa-bars handle"></i>
-		<i class="fa fa-minus minify"></i>
-		<a class='item-title' href="{{route('soda.'.$hint.'.view', ['id'=>$tree->id])}}">{{$tree->name}}</a>
-		<div class="btn-group pull-right" role="group" aria-label="Basic example">
-			<a data-tree-add class='btn btn-success btn-sm' href="{{route('soda.'.$hint.'.create', ['id'=>$tree->id])}}"><span class="fa fa-plus"></span></a>
-			<a data-tree-link class='btn btn-info btn-sm' href="{{$tree->slug}}"><span class="fa fa-external-link"></span></a>
-			<a data-tree-delete class='btn btn-danger btn-sm' href="{{route('soda.'.$hint.'.delete', ['id'=>$tree->id])}}"><span class="fa fa-remove"></span></a>
-		</div>
-		<div class="'clearfix" style="clear:both;"></div>
-	</div>
+<li class="tree-row" data-id="{{ $tree->id }}" data-move="{{route('soda.'.$hint.'.move')}}" style="display:{{ isset($display) ? $display : 'block' }}">
+    <div class="tree-item clearfix">
+        <span class="handle">
+            <img src="/soda/cms/img/drag-dots.gif" />
+        </span>
+        <span class="item-status">
+            <span class="{{ $tree->status == \Soda\Cms\Components\Status::DRAFT ? 'inactive' : 'active' }}-circle"></span>
+        </span>
+        <a class="item-title" href="{{ route('soda.'.$hint.'.view', ['id'=>$tree->id]) }}">
+            <span>{{ $tree->name }}</span>
+        </a>
+        <i class="fa fa-chevron-right minify" style="display:{{ $tree->hasChildrenRelation() && count($tree->children) > 0 ? 'inline' : 'none' }}"></i>
+        <div class="btn-group pull-right" role="group" aria-label="Basic example">
+            <a data-tree-add class="btn btn-success btn-sm" href="{{ route('soda.'.$hint.'.create', ['id'=>$tree->id]) }}">
+                <span class="fa fa-plus"></span>
+            </a>
+            <a data-tree-link class="btn btn-info btn-sm" href="{{ $tree->slug }}">
+                <span class="fa fa-external-link"></span>
+            </a>
+            <a data-tree-delete class="btn btn-danger btn-sm" href="{{ route('soda.'.$hint.'.delete', ['id' => $tree->id]) }}">
+               <span class="fa fa-remove"></span>
+            </a>
+        </div>
+    </div>
 
-	<ul>
-		@if ($tree->relationLoaded('children') && $tree->children->count() > 0)
-			@foreach($tree->children as $child)
-				@include('soda::partials.tree.tree', ['tree'=>$child])
-			@endforeach
-		@endif
-	</ul>
+    <ul class="tree-sub-items">
+        @if ($tree->hasChildrenRelation() && count($tree->children) > 0)
+            @foreach($tree->children as $child)
+                @include('soda::partials.tree.tree', ['tree' => $child, 'display' => 'none'])
+            @endforeach
+        @endif
+    </ul>
 </li>
