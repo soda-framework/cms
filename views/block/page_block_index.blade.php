ce309b8bee28264512b4c4bab93c387468f55e12
Ryan Brereton-Canham <ryan@madeinkatana.com>
2016-10-13 17:55:32 +1030

    Fixed table column

--- views/block/page_block_index.blade.php
+++ views/block/page_block_index.blade.php
@@ -3,39 +3,47 @@
 @section('wrapper-content')
 	<p>{{ $block->type->description}}</p>
 	<hr />
-	<table class="table">
-		<thead>
-		<tr>
-			@foreach($block->type->fields as $field)
-				@if($field->show_in_table)
-				<th>{{ $field->name }}</th>
-				@endif
-			@endforeach
-			<th>Options</th>
-		</tr>
-		</thead>
-		<tbody>
-		@foreach($models as $model)
-			<tr>
-				@foreach($block->type->fields as $field)
-					@if($field->show_in_table)
-                        <td>
-                            {!! SodaForm::field($field)->setModel($model)->renderForTable() !!}
+	@if(count($models))
+	<div class="table-fixed fixed-options-column">
+        <div class="table-fixed-inner">
+            <table class="table">
+                <thead>
+                <tr>
+                    @foreach($block->type->fields as $field)
+                        @if($field->show_in_table)
+                            <th>{{ $field->name }}</th>
+                        @endif
+                    @endforeach
+                    <th class="fixed-column options-column">Options</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($models as $model)
+                    <tr>
+                        @foreach($block->type->fields as $field)
+                            @if($field->show_in_table)
+                                <td>
+                                    {!! SodaForm::field($field)->setModel($model)->renderForTable() !!}
+                                </td>
+                            @endif
+                        @endforeach
+                        <td class="fixed-column options-column">
+                            <a href="{{route('soda.page.block.view', ['page_id' => $page->id, 'id'=>$model->id, 'type'=>$block->identifier])}}" class="btn btn-success btn-sm"><i class="fa fa-pencil"></i></a>
+                            @if($block->pivot->can_delete)
+                                <a href="{{route('soda.page.block.delete', ['page_id' => $page->id, 'id'=>$model->id, 'type'=>$block->identifier])}}" class="btn btn-danger btn-sm"><i class="fa fa-remove"></i></a>
+                            @endif
                         </td>
-					@endif
-				@endforeach
-				<td>
-					<a href="{{route('soda.page.block.view', ['page_id' => $page->id, 'id'=>$model->id, 'type'=>$block->identifier])}}" class="btn btn-success btn-sm"><i class="fa fa-pencil"></i></a>
-					@if($block->pivot->can_delete)
-					<a href="{{route('soda.page.block.delete', ['page_id' => $page->id, 'id'=>$model->id, 'type'=>$block->identifier])}}" class="btn btn-danger btn-sm"><i class="fa fa-remove"></i></a>
-					@endif
-				</td>
-			</tr>
-		@endforeach
-		</tbody>
-	</table>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    {!! $models->render() !!}
+    @else
+    <p>No records found.</p>
+    @endif
 
-	{!! $models->render() !!}
 	@if($block->pivot->can_create)
 	<a href="{{route('soda.page.block.view',['page_id' => $page->id, 'type' => $block->identifier])}}" class="btn btn-primary">Create</a>
 	@endif
