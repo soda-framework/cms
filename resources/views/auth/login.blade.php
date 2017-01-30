f3c3deb5ee423c6510973ccf3bb5a2fad35432f9
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-01-30 16:58:44 +1030

    Base styling/layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,80 +4,329 @@
     <title>Soda CMS | Login</title>
 @endsection
 
-@section('main-content')
+@section('head.css')
+    @parent
     <style>
-        .dialog-box {
-            transform: translateY(20px);
-            opacity: 0;
-            transition: opacity 1s ease, transform 1s ease;
+        .box {
+            position: relative;
+            width: 100%;
+            background: rgba(0, 0, 0, 0.5);
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            border-bottom: 0;
+            border-radius: 10px 10px 0 0;
+            padding: 50px;
+            z-index: 5;
         }
 
-        .dialog-box-wrapper.loaded .dialog-box {
-            transform: translateY(0px);
+        .box:before {
+            content: "";
+            height: 10px;
+            border-radius: 10px;
+            position: absolute;
+            top: -11px;
+            background: rgba(0, 0, 0, .6);
+            border-bottom-left-radius: 0;
+            border-bottom-right-radius: 0;
+            width: 95%;
+            left: 2.5%;
+            z-index: -1;
+        }
+
+        .input,
+        .input input,
+        .input .spin,
+        .button,
+        .button.login button,
+        .pass-forgot a {
+            transition: 300ms cubic-bezier(.4, 0, .2, 1);
+            -webkit-transition: 300ms cubic-bezier(.4, 0, .2, 1);
+            -ms-transition: 300ms cubic-bezier(.4, 0, .2, 1);
+        }
+
+        .input,
+        .input input,
+        .input .spin,
+        .button,
+        .button button {
+            width: 100%;
+        }
+
+        .input,
+        .button {
+            margin-top: 30px;
+            height: 70px;
+        }
+
+        .input,
+        .input input,
+        .button,
+        .button button {
+            position: relative;
+        }
+
+        .input input {
+            height: 60px;
+            top: 10px;
+            border: none;
+            background: transparent;
+        }
+
+        .input input,
+        .button button {
+            font-size: 24px;
+            color: rgba(255, 255, 255, 0.8);
+            font-weight: 300;
+        }
+
+        .input:before,
+        .input .spin {
+            width: 100%;
+            height: 1px;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+        }
+
+        .input .spin {
+            background: #0ad788;
+            z-index: 4;
+            width: 0;
+        }
+
+        .input:before {
+            content: "";
+            background: rgba(0, 0, 0, 0.1);
+            z-index: 3;
+        }
+
+        .button.login {
+            z-index: 2;
+            width: 100%;
+            height: auto;
+            margin: 0px;
+        }
+
+        .button.login button {
+            width: 100%;
+            padding: 19px 0;
+            background: rgba(0, 0, 0, 0.6);
+            font-weight: 900;
+            font-size: 18px;
+            color: rgba(255, 255, 255, 0.4);
+            border: 1px solid rgba(0, 0, 0, 0.1);
+            border-top: 2px transparent;
+            border-radius: 0px 0px 10px 10px;
+            cursor: pointer;
+            z-index: 2;
+        }
+
+        .button.login button.active span {
+            opacity: 0;
+            transform: scale(0);
+            -webkit-transform: scale(0);
+            -ms-transform: scale(0);
+        }
+
+        .button.login button:hover {
+            background-color: #0ad788;
+            border-color: #25b37c;
+            border-top: 2px transparent;
+            color: #fff;
+        }
+
+        .pass-forgot {
+            width: 100%;
+            text-align: center;
+            margin-top: 10px;
+        }
+
+        .pass-forgot a {
+            color: rgba(0, 0, 0, 0.35);
+            font-size: 14px;
+        }
+
+        .pass-forgot a:hover, .pass-forgot a:active, .pass-forgot a:focus {
+            color: rgba(0, 0, 0, 0.55);
+        }
+
+        body {
+            background-image: url(http://sf.co.ua/2012/wallpaper-2077534.jpg);
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+
+        .materialContainer {
+            width: 100%;
+            max-width: 540px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+        }
+
+        input:focus {
+            outline: none !important;
+        }
+
+        /*
+ * Default jvFloat theme.
+ * modify it as you wish!
+ */
+
+        .jvFloat {
+            position: relative;
+            display: inline;
+            margin-top: 1em;
+        }
+
+        .jvFloat .placeHolder.required {
+            color: red;
+        }
+
+        /* Start CSS3 Animations on supported browser */
+        .jvFloat .placeHolder {
+            position: absolute;
+            top: -10px;
+            left: 0;
+            width: auto;
+            font-size: 14px;
+            color: rgba(255, 255, 255, 0.5);
+            font-weight: normal;
+            -webkit-transform: translate(0, 0);
+            -moz-transform: translate(0, 0);
+            -o-transform: translate(0, 0);
+            -ms-transform: translate(0, 0);
+            transform: translate(0, 0);
+            -webkit-transition: -webkit-transform 150ms, opacity 100ms, visibility 100ms;
+            transition: transform 150ms, opacity 100ms, visibility 100ms;
+            opacity: 0;
+            visibility: hidden;
+        }
+
+        /*Allows textarea floating placeholder to be positioned distinctly from the normal .placeHolder class
+         * This is required when working with Multiline text areas
+         */
+        .jvFloat .placeHolder.textarea {
+            /*top: 0px;*/
+        }
+
+        .jvFloat .placeHolder.active {
+            display: block;
+            visibility: visible;
+            -webkit-transform: translate(0, -1em);
+            -moz-transform: translate(0, -1em);
+            -o-transform: translate(0, -1em);
+            -ms-transform: translate(0, -1em);
+            transform: translate(0, -1em);
+            -webkit-transition: -webkit-transform 100ms, opacity 120ms, visibility 120ms;
+            transition: transform 100ms, opacity 120ms, visibility 120ms;
             opacity: 1;
         }
+
+        /* End CSS3 */
+
+        /* Legacy browser */
+        /*.jvFloat .placeHolder {
+            position: absolute;
+            top: -1em;
+            left: 0;
+            color: #0c61fc;
+            font-size: .85em;
+            font-weight: bold;
+            opacity: 0;
+            visibility: hidden;
+        }
+        .jvFloat .placeHolder.active {
+            display: block;
+            visibility: visible;
+            opacity: 1;
+        }*/
+        /* End Legacy */
+
+        input:-webkit-autofill,
+        input:-webkit-autofill:hover,
+        input:-webkit-autofill:focus,
+        input:-webkit-autofill:active {
+            background: none !important;
+            background-color: transparent !important;
+            transition: background-color 5000s ease-in-out 0s !important;
+            color: rgba(255, 255, 255, 0.8) !important;
+            -webkit-text-fill-color: rgba(255, 255, 255, 0.8) !important;
+        }
+
+        .soda-logo {
+            text-align: center;
+            margin-bottom: 10px;
+        }
+
+        .soda-logo img {
+            max-height: 55px;
+        }
+
     </style>
-    <div class="dialog-box-wrapper">
-        <div class="dialog-box">
-            <div class="dialog-box-inner">
-                <div class="soda-logo-wrap">
-                    <div class="soda-logo">
-                        <img src="/soda/cms/img/sodacms_droplime.png"/><br/>
-                        <img src="/soda/cms/img/sodacms_logowhite.png"/>
-                    </div>
+@stop
+
+@section('main-content')
+    <form method="POST">
+        {!! csrf_field() !!}
+        <div class="materialContainer">
+            <div class="box">
+                <div class="soda-logo">
+                    <img src="/soda/cms/img/sodacms_droplime.png">
+                    <img src="/soda/cms/img/sodacms_logowhite.png">
                 </div>
-                <form class="form-horizontal" role="form" method="POST" action="{{ route('soda.login-attempt') }}">
-                    {!! csrf_field() !!}
 
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} row">
-                        <div class="input-group">
-                            <label class="input-group-addon" for="email"><i class="fa fa-user"></i></label>
-                            <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}"
-                                   placeholder="Enter your email address...">
-                        </div>
+                <div class="input">
+                    <input type="text" name="email" placeholder="Username">
+                    <span class="spin"></span>
+                </div>
 
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                        @endif
-                    </div>
+                <div class="input">
+                    <input type="password" name="password" placeholder="Password">
+                    <span class="spin"></span>
+                </div>
+            </div>
 
-                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} row">
-                        <div class="input-group">
-                            <label class="input-group-addon" for="password"><i class="fa fa-unlock-alt"></i></label>
-                            <input type="password" class="form-control" name="password" id="password"
-                                   placeholder="Enter your password...">
-                        </div>
+            <div class="button login">
+                <button><span>LOGIN</span></button>
+            </div>
 
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                        @endif
-                    </div>
-
-                    <div class="form-group row">
-                        <button type="submit" class="btn btn-dialog btn-block">
-                            Login
-                        </button>
-                    </div>
-                    {{--
-                    <p class="text-center">
-                        <a href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                    </p>
-                    --}}
-                </form>
+            <div class="pass-forgot">
+                <a href="#">I forgot my password</a>
             </div>
         </div>
-    </div>
+    </form>
 @endsection
 
 @section('footer.js')
     @parent
     <script>
         $(function () {
-            $('.dialog-box-wrapper').addClass('loaded');
+            $('input').jvFloat();
+
+            $(".input input").on('focus', function () {
+                $(this).closest('.input').find(".spin").css({
+                    "width": "100%"
+                });
+            }).on('blur', function () {
+                $(this).closest('.input').find(".spin").css({
+                    "width": "0px"
+                });
+            });
+
+            setTimeout(function () {
+                try {
+                    $('input:autofill').siblings('.placeHolder').addClass('active');
+                } catch (error) {
+                }
+                try {
+                    $('input:-webkit-autofill').siblings('.placeHolder').addClass('active');
+                } catch (error) {
+                }
+            }, 500);
         });
     </script>
 @stop
