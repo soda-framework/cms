d1c1083011a3556b7080f08222fc00bbbc174e00
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-02-01 10:09:32 +1030

    Placeholder styling hacks

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,40 +6,42 @@
 
 @section('main-content')
     <form method="POST" class="soda-login__container">
-        {!! csrf_field() !!}
-        <div class="soda-login__box">
-            <div class="soda-login__logo">
-                <img src="/soda/cms/img/sodacms_droplime.png">
-                <img src="/soda/cms/img/sodacms_logowhite.png">
+        <div class="fadeInUp">
+            {!! csrf_field() !!}
+            <div class="soda-login__box">
+                <div class="soda-login__logo">
+                    <img src="/soda/cms/img/sodacms_droplime.png">
+                    <img src="/soda/cms/img/sodacms_logowhite.png">
+                </div>
+
+                <div class="soda-login__errors">
+                    @if ($errors->has('email'))
+                        <span class="error">{{ $errors->first('email') }}</span>
+                    @endif
+
+                    @if ($errors->has('password'))
+                        <span class="error">{{ $errors->first('password') }}</span>
+                    @endif
+                </div>
+
+                <div class="soda-login__input">
+                    <input type="text" name="email" placeholder="Email Address">
+                    <span class="soda-login__underline"></span>
+                </div>
+
+                <div class="soda-login__input">
+                    <input type="password" name="password" placeholder="Password">
+                    <span class="soda-login__underline"></span>
+                </div>
             </div>
 
-            <div class="soda-login__errors">
-                @if ($errors->has('email'))
-                    <span class="error">{{ $errors->first('email') }}</span>
-                @endif
-
-                @if ($errors->has('password'))
-                    <span class="error">{{ $errors->first('password') }}</span>
-                @endif
+            <div class="soda-login__button-container">
+                <button><span>LOGIN</span></button>
             </div>
 
-            <div class="soda-login__input">
-                <input type="text" name="email" placeholder="Email Address">
-                <span class="soda-login__underline"></span>
+            <div class="soda-login__pass-forgot">
+                <a href="#">I forgot my password</a>
             </div>
-
-            <div class="soda-login__input">
-                <input type="password" name="password" placeholder="Password">
-                <span class="soda-login__underline"></span>
-            </div>
-        </div>
-
-        <div class="soda-login__button-container">
-            <button><span>LOGIN</span></button>
-        </div>
-
-        <div class="soda-login__pass-forgot">
-            <a href="#">I forgot my password</a>
         </div>
     </form>
 @endsection
@@ -48,8 +50,6 @@
     @parent
     <script>
         $(function () {
-            $('.soda-login__input input').jvFloat();
-
             $(".soda-login__input input").on('focus', function () {
                 $(this).closest('.soda-login__input').find(".soda-login__underline").css({
                     "width": "100%"
@@ -60,16 +60,44 @@
                 });
             });
 
-            setTimeout(function () {
-                try {
-                    $('.soda-login__input input:autofill').siblings('.placeHolder').addClass('active');
-                } catch (error) {
+            // AUTOFILL STYLING REMOVAL HACK
+            var _retries = 0;
+            var _interval = window.setInterval(function ()
+            {
+                var autofills;
+                var terminateAutofillInterval = function() {
+                    $('.soda-login__input input').jvFloat();
+                    window.clearInterval(_interval); // stop polling
                 }
+
                 try {
-                    $('.soda-login__input input:-webkit-autofill').siblings('.placeHolder').addClass('active');
+                    autofills = $('.soda-login__input input:-webkit-autofill');
                 } catch (error) {
+                    try {
+                        autofills = $('.soda-login__input input:autofill');
+                    } catch (error) {
+                        terminateAutofillInterval();
+                    }
                 }
-            }, 500);
+
+                if (autofills.length > 0) {
+                    var _clones = [];
+                    autofills.each(function() {
+                        var clone = $(this).clone(true, true);
+                        $(this).after(clone).remove();
+                        _clones.push(clone);
+                    });
+
+                    terminateAutofillInterval();
+
+                    $.each(_clones, function(index, _clone) {
+                        _clone.siblings('.placeHolder').addClass('active');
+                    })
+
+                } else if (_retries++ > 20) {
+                    terminateAutofillInterval();
+                }
+            }, 50);
         });
     </script>
 @stop
