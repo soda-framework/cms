3f388ffaba4bea1076de88af1df664e8ebb56c09
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-02-08 10:45:04 +1030

    Remove image options from Tinymce

--- src/Foundation/Forms/Fields/Tinymce.php
+++ src/Foundation/Forms/Fields/Tinymce.php
@@ -14,12 +14,12 @@ class Tinymce extends AbstractFormField
             'selector'     => '#field_'.$this->getFieldName(),
             'height'       => 500,
             'plugins'      => [
-                'advlist autolink lists link image charmap print hr anchor pagebreak',
+                'advlist autolink lists link charmap print hr anchor pagebreak',
                 'searchreplace wordcount visualblocks visualchars code fullscreen',
                 'insertdatetime media nonbreaking save table contextmenu directionality',
                 'emoticons template paste textcolor colorpicker textpattern imagetools',
             ],
-            'toolbar1'     => 'insertfile undo redo | forecolor backcolor styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            'toolbar1'     => 'undo redo | forecolor backcolor styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link',
             'image_advtab' => true,
             'convert_urls' => false, //prevent "http://site.com/my/directory" becoming "/my/directory" - https://www.tinymce.com/docs/configure/url-handling/
         ];
