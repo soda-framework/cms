32305184c73abf9542ce1cf6372ec1b515d2f9d7
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-03-07 09:47:31 +1030

    nav tab styling

--- resources/views/data/pages/view.blade.php
+++ resources/views/data/pages/view.blade.php
@@ -32,50 +32,31 @@ if ($page->type && $page->type->blockTypes) {
     'description' => $page->description,
 ])
 
-@section('tab.settings')
-    <div class="row">
-        @if(!$smallView)
-        <div class="col-md-9 col-xs-12">
-            <div class="content-block">
-                @if($page->type && $page->type->description)
-                    <p>{{ $page->type->description }}</p>
-                    <hr/>
-                @endif
-                @if($page->type && $page->type->fields)
-                    @foreach($page->type->fields->where('pivot.show_in_table', 1) as $field)
-                        {!! SodaForm::field($field)->setModel($page->pageAttributes())->setPrefix('settings') !!}
-                    @endforeach
-                @endif
-            </div>
-        </div>
-        @endif
-        <div class="{{ $smallView ? 'col-md-12' : 'col-md-3 pull-right' }} col-xs-12">
-            <div class="content-block">
-                {!! SodaForm::text([
-                    "name"        => "Name",
-                    "description" => "The name of this page",
-                    "field_name"  => 'name',
-                ])->setLayout($smallView ? soda_cms_view_path('partials.inputs.layouts.inline') : soda_cms_view_path('partials.inputs.layouts.stacked'))->setModel($page) !!}
+@section('settings.basic')
+    <div class="content-block">
+        {!! SodaForm::text([
+            "name"        => "Name",
+            "description" => "The name of this page",
+            "field_name"  => 'name',
+        ])->setLayout($smallView ? soda_cms_view_path('partials.inputs.layouts.inline') : soda_cms_view_path('partials.inputs.layouts.stacked'))->setModel($page) !!}
 
-                {!! SodaForm::slug([
-                    'name'        => 'Slug',
-                    'description' => 'The url of this page',
-                    'field_name'  => 'slug',
-                    'field_params' => [
-                        'prefix' => ($page->parent_id !== null && $parent = $page->getParent()) ? $parent->slug : '',
-                    ],
-                ])->setLayout($smallView ? soda_cms_view_path('partials.inputs.layouts.inline') : soda_cms_view_path('partials.inputs.layouts.stacked'))->setModel($page) !!}
+        {!! SodaForm::slug([
+            'name'        => 'Slug',
+            'description' => 'The url of this page',
+            'field_name'  => 'slug',
+            'field_params' => [
+                'prefix' => ($page->parent_id !== null && $parent = $page->getParent()) ? $parent->slug : '',
+            ],
+        ])->setLayout($smallView ? soda_cms_view_path('partials.inputs.layouts.inline') : soda_cms_view_path('partials.inputs.layouts.stacked'))->setModel($page) !!}
 
-                {!! SodaForm::toggle([
-                    'name'         => 'Published',
-                    'field_name'   => 'status',
-                    'value'        => Soda\Cms\Foundation\Constants::STATUS_LIVE,
-                    'field_params' => [
-                        'checked-value'   => Soda\Cms\Foundation\Constants::STATUS_LIVE,
-                        'unchecked-value' => Soda\Cms\Foundation\Constants::STATUS_DRAFT],
-                ])->setLayout($smallView ? soda_cms_view_path('partials.inputs.layouts.inline') : soda_cms_view_path('partials.inputs.layouts.stacked'))->setModel($page) !!}
-            </div>
-        </div>
+        {!! SodaForm::toggle([
+            'name'         => 'Published',
+            'field_name'   => 'status',
+            'value'        => Soda\Cms\Foundation\Constants::STATUS_LIVE,
+            'field_params' => [
+                'checked-value'   => Soda\Cms\Foundation\Constants::STATUS_LIVE,
+                'unchecked-value' => Soda\Cms\Foundation\Constants::STATUS_DRAFT],
+        ])->setLayout($smallView ? soda_cms_view_path('partials.inputs.layouts.inline') : soda_cms_view_path('partials.inputs.layouts.stacked'))->setModel($page) !!}
     </div>
 @stop
 
@@ -141,72 +122,97 @@ if ($page->type && $page->type->blockTypes) {
 @stop
 
 @section('content')
-    <ul class="nav nav-tabs" role="tablist">
-        <li role='presentation' aria-controls="tab_settings">
-            <a role="tab" data-toggle="tab" href="#tab_settings">Settings</a>
-        </li>
-
-        @foreach($blockTypes as $blockType)
-            @if($blockType->list_action_type == 'view')
-                <li role='presentation' aria-controls="tab_{{ strtolower($blockType->identifier) }}">
-                    <a role="tab" data-toggle="tab" href="#tab_{{ strtolower($blockType->identifier) }}">{{ $blockType->name }}</a>
-                </li>
-            @endif
-        @endforeach
-
-        @permission("advanced-pages")
-        <li role='presentation' aria-controls="tab_advanced">
-            <a role="tab" data-toggle="tab" href="#tab_advanced">Advanced</a>
-        </li>
-        @endpermission
-
-        @if($page->id)
-        @permission("attach-blocks")
-        <li role='presentation'>
-            <a role="tab" href="#tab_new-block">+</a>
-        </li>
-        @endpermission
-        @endif
-    </ul>
-
-    <form method="POST" id="page-form" action="{{ route('soda.pages.' . ($page->id ? 'update' : 'store'), $page->id) }}" enctype="multipart/form-data">
+    <form method="POST" id="page-form" action="{{ route('soda.pages.' . ($page->id ? 'update' : 'store'), $page->id) }}"
+          enctype="multipart/form-data">
         {!! csrf_field() !!}
         {!! method_field($page->id ? 'PUT' : 'POST') !!}
         @if($page->type)
             <input type="hidden" name="page_type_id" value="{{ $page->type->id }}"/>
         @endif
         @if(!$page->id)
-        <input type="hidden" name="parent_id" value="{{ $page->parent_id }}"/>
+            <input type="hidden" name="parent_id" value="{{ $page->parent_id }}"/>
         @endif
-        <div class="tab-content">
-            <div class="tab-pane" id="tab_settings" role="tabpanel">
-                @yield('tab.settings')
-            </div>
-            @foreach($blockTypes as $blockType)
-                @if($blockType->list_action_type == 'view')
-                    <div class="tab-pane" id="tab_{{ strtolower($blockType->identifier) }}" role="tabpanel">
+        <div class="row">
+            <div class="{{ !$smallView ? 'col-md-9' : 'col-md-12' }} col-xs-12">
+
+                <ul class="nav nav-tabs" role="tablist">
+                    <li role='presentation' aria-controls="tab_settings">
+                        <a role="tab" data-toggle="tab" href="#tab_settings">Settings</a>
+                    </li>
+
+                    @foreach($blockTypes as $blockType)
+                        @if($blockType->list_action_type == 'view')
+                            <li role='presentation' aria-controls="tab_{{ strtolower($blockType->identifier) }}">
+                                <a role="tab" data-toggle="tab"
+                                   href="#tab_{{ strtolower($blockType->identifier) }}">{{ $blockType->name }}</a>
+                            </li>
+                        @endif
+                    @endforeach
+
+                    @permission("advanced-pages")
+                    <li role='presentation' aria-controls="tab_advanced">
+                        <a role="tab" data-toggle="tab" href="#tab_advanced">Advanced</a>
+                    </li>
+                    @endpermission
+
+                    @if($page->id)
+                        @permission("attach-blocks")
+                        <li role='presentation'>
+                            <a role="tab" href="#tab_new-block">+</a>
+                        </li>
+                        @endpermission
+                    @endif
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane" id="tab_settings" role="tabpanel">
+                        @if(!$smallView)
                         <div class="content-block">
-                            @include($blockType->list_action, [
-                                'blockType'  => $blockType,
-                                'page'       => $page,
-                                'blocks'     => $page->block($blockType)->paginate(null, ['*'], $blockType->identifier .'-page')
-                            ])
+                            @if($page->type && $page->type->description)
+                                <p>{{ $page->type->description }}</p>
+                                <hr/>
+                            @endif
+                            @if($page->type && $page->type->fields)
+                                @foreach($page->type->fields->where('pivot.show_in_table', 1) as $field)
+                                    {!! SodaForm::field($field)->setModel($page->pageAttributes())->setPrefix('settings') !!}
+                                @endforeach
+                            @endif
                         </div>
+                        @else
+                            @yield('settings.basic')
+                        @endif
                     </div>
-                @endif
-            @endforeach
+                    @foreach($blockTypes as $blockType)
+                        @if($blockType->list_action_type == 'view')
+                            <div class="tab-pane" id="tab_{{ strtolower($blockType->identifier) }}" role="tabpanel">
+                                <div class="content-block">
+                                    @include($blockType->list_action, [
+                                        'blockType'  => $blockType,
+                                        'page'       => $page,
+                                        'blocks'     => $page->block($blockType)->paginate(null, ['*'], $blockType->identifier .'-page')
+                                    ])
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
 
-            @permission("live-preview")
-            <div class="tab-pane" id="tab_live" role="tabpanel">
-                @yield('tab.live-preview')
-            </div>
-            @endpermission
+                    @permission("live-preview")
+                    <div class="tab-pane" id="tab_live" role="tabpanel">
+                        @yield('tab.live-preview')
+                    </div>
+                    @endpermission
 
-            @permission("advanced-pages")
-            <div class="tab-pane" id="tab_advanced" role="tabpanel">
-                @yield('tab.advanced')
+                    @permission("advanced-pages")
+                    <div class="tab-pane" id="tab_advanced" role="tabpanel">
+                        @yield('tab.advanced')
+                    </div>
+                    @endpermission
+                </div>
             </div>
-            @endpermission
+            @if(!$smallView)
+            <div class="col-md-3 col-xs-12 pull-right" style="margin-top: 42px;">
+                @yield('settings.basic')
+            </div>
+            @endif
         </div>
     </form>
 
@@ -252,7 +258,7 @@ if ($page->type && $page->type->blockTypes) {
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            <button  class="btn btn-primary">Add Block</button>
+                            <button class="btn btn-primary">Add Block</button>
                         </div>
                     </form>
                 </div>
@@ -265,8 +271,8 @@ if ($page->type && $page->type->blockTypes) {
 @section('footer.js')
     @parent
     <script>
-        $(function() {
-            $('a[href="#tab_new-block"]').on('click', function(e) {
+        $(function () {
+            $('a[href="#tab_new-block"]').on('click', function (e) {
                 e.preventDefault();
                 $('#newBlockModal').modal('show')
             })
