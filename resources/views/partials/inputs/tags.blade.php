027d005057d1c1e3ab000481d27aeb60039c943c
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-05-30 16:29:38 +0930

    Fix select2 tags, keeping the dropdown hidden

--- resources/views/partials/inputs/tags.blade.php
+++ resources/views/partials/inputs/tags.blade.php
@@ -15,8 +15,23 @@
             $('#{{ $field_id }}').select2({
                 tags: true,
                 multiple: true,
-                //dropdownCssClass: 'hide',
+                selectOnClose: true,
+                matcher: function(searchParams, data) {
+                    // This bit taken from Select2's default matcher
+                    var match = $.extend(true, {}, data);
+
+                    // Don't partial match tags, otherwise if a user has a tag 'abc' it is
+                    // impossible to then create a tag 'ab'.
+                    if (searchParams.term === data.text)
+                        return match;
+                    return null;
+                },
+                dropdownCssClass: 'hide',
                 {!! app('soda.form')->buildJsParams($field_parameters['settings']) !!}
+            });
+
+            $('#{{ $field_id }}').on('select2:unselect', function(e){
+                $(e.params.data.element).remove();
             });
         });
     </script>
