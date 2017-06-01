08dfa9e073b5f360e5f752141867eae2a60d74c8
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-06-01 09:49:36 +0930

    Publish timezone config

--- config/publish/cms.php
+++ config/publish/cms.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'hint'    => 'soda',
-    'path'    => 'cms',
-    'https'   => false,
-    'form'    => [
+    'hint'             => 'soda',
+    'path'             => 'cms',
+    'publish_timezone' => 'Australia/Sydney',
+    'https'            => false,
+    'form'             => [
         'default-layout'    => 'soda::partials.inputs.layouts.stacked',
         'default-view-path' => 'soda::partials.inputs',
     ],
