f24b24cb019d31f707f10c60be01987793a0bc8c
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-08-24 13:11:42 +0930

    Hide quicklinks dashboard item if none set

--- resources/views/partials/dashboard/quicklinks.blade.php
+++ resources/views/partials/dashboard/quicklinks.blade.php
@@ -1,11 +1,17 @@
-<div class="col-xs-12">
-    <h2>@lang('soda::terminology.quicklink_plural')</h2>
-    @foreach(app('soda.interface')->quicklinks() as $quicklink)
-        <a href="{{ $quicklink->getUrl() }}" class="tag">
-          <span>{{ $quicklink->text }}</span>
-          @if($quicklink->user_id == Auth::guard('soda')->user()->id)
-          <span class="tag-delete">&times;</span>
-          @endif
-        </a>
-    @endforeach
-</div>
+<?php
+$quicklinks = app('soda.interface')->quicklinks();
+?>
+
+@if(count($quicklinks))
+    <div class="col-xs-12">
+        <h2>@lang('soda::terminology.quicklink_plural')</h2>
+        @foreach($quicklinks as $quicklink)
+            <a href="{{ $quicklink->getUrl() }}" class="tag">
+                <span>{{ $quicklink->text }}</span>
+                @if($quicklink->user_id == Auth::guard('soda')->user()->id)
+                    <span class="tag-delete">&times;</span>
+                @endif
+            </a>
+        @endforeach
+    </div>
+@endif
