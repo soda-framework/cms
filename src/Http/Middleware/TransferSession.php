26998677059a8a611b21d264611afb1bc9a209fa
Ryan Brereton-Canham <ryan@madeinkatana.com>
2017-10-20 15:58:14 +1030

    Code cleanup

--- src/Http/Middleware/TransferSession.php
+++ src/Http/Middleware/TransferSession.php
@@ -8,27 +8,50 @@ class TransferSession
 {
     public function handle($request, Closure $next)
     {
-        if($request->has('sid') && $request->has('validate')) {
-            $oldSessionId = $request->session()->getId();
+        if ($request->has('sid') && $request->has('validate')) {
+            $sessionId = $request->input('sid');
 
-            $request->session()->setId($request->input('sid'));
-            $request->session()->start();
+            if ($this->validateSession($request, $sessionId, $request->input('validate'))) {
+                $this->switchSession($request, $sessionId);
 
-            if($request->session()->get('redirect-validate') === $request->input('validate')) {
-                $currentQuery = parse_url($request->fullUrl(), PHP_URL_QUERY);
-                parse_str($currentQuery, $currentQueryArray);
-
-                unset($currentQueryArray['sid'], $currentQueryArray['validate']);
-
-                $currentQuery = http_build_query($currentQueryArray);
-
-                return redirect()->to($request->url() . (count($currentQueryArray) ? "?$currentQuery" : ''));
+                return redirect()->to($this->removeSessionQueryParameters($request));
             }
-
-            $request->session()->setId($oldSessionId);
-            $request->session()->start();
         }
 
         return $next($request);
+    }
+
+    protected function validateSession($request, $sessionId, $validationToken)
+    {
+        $sessionData = $this->parseSessionById($request, $sessionId);
+
+        if (is_array($sessionData) && isset($sessionData['redirect-validate']) && $sessionData['redirect-validate'] == $validationToken) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function parseSessionById($request, $sessionId)
+    {
+        return @unserialize($request->session()->getHandler()->read($sessionId));
+    }
+
+    protected function switchSession($request, $sessionId)
+    {
+        $request->session()->setId($request->input('sid'));
+        $request->session()->start();
+    }
+
+    protected function removeSessionQueryParameters($request)
+    {
+        $currentQuery = parse_url($request->fullUrl(), PHP_URL_QUERY);
+        parse_str($currentQuery, $currentQueryArray);
+
+        unset($currentQueryArray['sid'], $currentQueryArray['validate']);
+
+        $currentQuery = http_build_query($currentQueryArray);
+
+        return $request->url() . (count($currentQueryArray) ? "?$currentQuery" : '');
     }
 }
